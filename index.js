e06b9acff146b5bd75f25a50d64d0cb023f0efcd
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-05-25 22:41:48 -0300

    feat: add middleware of error

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const app = require('./app');
 require('dotenv').config();
+const middlewares = require('./middlewares');
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
+app.use(middlewares.error);
 app.use('/products', require('./routes/products'));
 
 app.listen(process.env.PORT, () => {
