8a528803c1e8800897d686a259486ecd69a811b4
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-05-25 22:44:28 -0300

    feat: add productsModel

---
+++ models/productsModel.js
@@ -0,0 +1,12 @@
+const connection = require('../db');
+
+const getAll = () => connection.execute('SELECT * FROM StoreManager.products');
+
+const getById = (id) => connection.execute(
+  'SELECT * FROM StoreManager.products WHERE id = ?', [id],
+);
+
+module.exports = {
+  getAll,
+  getById,
+};
