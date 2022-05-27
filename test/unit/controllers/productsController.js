57b0880f218a8076d3c1f65875c1bcc8cc41bee7
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-05-27 20:36:20 -0300

    test: add test to method DELETE at Controller layer

--- test/unit/controllers/productsController.js
+++ test/unit/controllers/productsController.js
@@ -108,3 +108,24 @@ describe('quando é atualizado com sucesso', async () => {
   });
 });
 
+describe('quando é removido com sucesso', async () => {
+  const response = {};
+  const request = { params: { id: 1} };
+
+  before(() => {
+    response.status = sinon.stub().returns(response);
+    response.end = sinon.stub().returns();
+
+    sinon.stub(ProductsService, "remove").resolves();
+  });
+
+  after(() => {
+    ProductsService.remove.restore();
+  });
+
+  it('Verifica se é chamado o status com o código 204', async () => {
+    await ProductsController.deleteProducts(request, response);
+
+    expect(response.status.calledWith(204)).to.be.equal(true);
+  });
+});
