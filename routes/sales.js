b9d3426f2f7e088da734246e4360f3f75bb02484
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-05-30 13:42:15 -0300

    feat: add method POST in route Sales

--- routes/sales.js
+++ routes/sales.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getSales } = require('../controllers/salesController');
+const { getSales, createSales } = require('../controllers/salesController');
+const { salesValidation } = require('../middlewares');
 
 const router = express.Router();
 
 router.get('/', getSales);
 router.get('/:id', getSales);
+router.post('/', salesValidation, createSales);
 
 module.exports = router;
