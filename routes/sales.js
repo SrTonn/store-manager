55fc2c2d146dfe86efa7ef4fc9e642797c5d59bd
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-05-30 17:31:36 -0300

    feat: add method DELETE in route Sales

--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const { getSales, createSales, updateSales } = require('../controllers/salesController');
+const {
+  getSales,
+  createSales,
+  updateSales,
+  deleteSales } = require('../controllers/salesController');
 const { salesValidation } = require('../middlewares');
 
 const router = express.Router();
@@ -8,5 +12,6 @@ router.get('/', getSales);
 router.get('/:id', getSales);
 router.post('/', salesValidation, createSales);
 router.put('/:id', salesValidation, updateSales);
+router.delete('/:id', deleteSales);
 
 module.exports = router;
