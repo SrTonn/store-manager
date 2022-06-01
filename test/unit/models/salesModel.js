c567c0b14c847113b6430f08d55ad0d2f1a34a0d
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-06-01 12:08:14 -0300

    test: add test to method DELETE at Model layer

--- test/unit/models/salesModel.js
+++ test/unit/models/salesModel.js
@@ -22,3 +22,21 @@ describe("Atualiza uma venda no BD", () => {
     expect(response).to.be.a("array");
   });
 });
+
+describe("Remove uma venda no BD", () => {
+  const ID_EXAMPLE = 1;
+
+  before(() => {
+    sinon.stub(connection, "execute").resolves();
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
+  it("Verifica se retorna \"undefined\"", async () => {
+    const response = await SalesModel.removeSale(ID_EXAMPLE);
+
+    expect(response).to.be.undefined;
+  });
+});
