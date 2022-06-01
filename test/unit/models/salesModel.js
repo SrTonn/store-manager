e2006bdb6476492a2156c65ff5c2027b35a07931
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-06-01 12:09:17 -0300

    test: add test to method GET at Model layer

--- test/unit/models/salesModel.js
+++ test/unit/models/salesModel.js
@@ -71,3 +71,116 @@ describe("insere uma nova venda no BD", () => {
   });
 });
 
+describe("quando consulta por todos itens", () => {
+  before(() => {
+    const resultExecute = [
+      {
+        saleId: 1,
+        productId: 1,
+        quantity: 5,
+        date: "2022-06-01T01:33:55.000Z"
+      },
+      {
+        saleId: 1,
+        productId: 2,
+        quantity: 10,
+        date: "2022-06-01T01:33:55.000Z"
+      },
+      {
+        saleId: 2,
+        productId: 3,
+        quantity: 15,
+        date: "2022-06-01T01:33:55.000Z"
+      }
+    ];
+
+    sinon.stub(connection, "execute").resolves(resultExecute);
+  })
+
+  after(() => {
+    connection.execute.restore();
+  })
+
+  it("Verifica se retorna um array", async () => {
+    const response = await SalesModel.getAll();
+
+    expect(response).to.be.an("array");
+  });
+
+  it("Verifica se o array não está vazio", async () => {
+    const response = await SalesModel.getAll();
+
+    expect(response).to.not.be.empty;
+  });
+
+  it("Verifica se o contém 3 items", async () => {
+    const response = await SalesModel.getAll();
+
+    expect(response.length).to.be.equal(3)
+  });
+
+  it("Verifica se o array possui itens do tipo objeto", async () => {
+    const [item] = await SalesModel.getAll();
+
+    expect(item).to.be.an("object");
+  });
+
+  it("Verifica se tais itens possuem as propriedades: \"productId\", \"quantity\" e \"date\"",
+  async () => {
+    const [item] = await SalesModel.getAll();
+
+    expect(item).to.include.all.keys(
+      "productId",
+      "quantity",
+      "date",
+    );
+  });
+
+});
+
+describe("quando consulta por uma venda", () => {
+  before(() => {
+    const resultExecute = [
+      {
+        productId: 1,
+        quantity: 5,
+        date: "2022-06-01T01:33:55.000Z"
+      },
+      {
+        productId: 2,
+        quantity: 10,
+        date: "2022-06-01T01:33:55.000Z"
+      }
+    ];
+
+    sinon.stub(connection, "execute").resolves(resultExecute);
+  })
+
+  after(() => {
+    connection.execute.restore();
+  })
+
+  it("Verifica se retorna um array", async () => {
+    const response = await SalesModel.getById(1);
+
+    expect(response).to.be.an("array");
+  });
+
+  it("Verifica se o array não está vazio", async () => {
+    const response = await SalesModel.getById(1);
+
+    expect(response).to.not.be.empty;
+  });
+
+  it("Verifica se o contém 2 items", async () => {
+    const response = await SalesModel.getById(1);
+
+    expect(response.length).to.be.equal(2);
+  });
+
+  it("Verifica se o array possui itens do tipo objeto", async () => {
+    const [item] = await SalesModel.getById(1);
+
+    expect(item).to.be.an("object");
+  });
+});
